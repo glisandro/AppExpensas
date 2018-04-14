45a28a7308fa133783733ef3504f6a4152804eba
Gustavo L. Martinez <gustavomartinez@gmail.com>
2018-04-14 19:48:30 -0300

    Global scope for consorcio model

--- app/Consorcio.php
+++ app/Consorcio.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class Consorcio extends Model
 {
-    //
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('team_id', function (Builder $builder) {
+
+            // El consorcio siempre debe pertenecer al equipo actual
+            $builder->where('team_id', Auth::user()->currentTeam()->id);
+        });
+    }
 }
