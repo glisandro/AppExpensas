f13ab2b68ce28ca122083fbed0fe2df3ef546e77
Gustavo L. Martinez <gustavomartinez@gmail.com>
2018-04-14 17:41:38 -0300

    Add Propiedad

---
+++ resources/views/settings/propiedad/index.blade.php
@@ -0,0 +1,24 @@
+@extends('spark::layouts.app')
+
+@section('content')
+    <home :user="user" inline-template>
+        <div class="container">
+            <!-- Application Dashboard -->
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card card-default">
+                        <div class="card-header">{{__('Propiedades')}}</div>
+
+                        <div class="card-body">
+                            <ul>
+                            @foreach($propiedades as $propiedad)
+                                    <li><a href="/settings/consorcios/{{ $propiedad->id }}/propiedades">{{ $propiedad->denominacion }}</a></li>
+                            @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </home>
+@endsection
