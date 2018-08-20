52fcd6b12cfdee81427af46092c6a5e80a4a8a64
DESKTOP-23QUUHO\Gustavo <gustavomartinez@gmail.com>
2018-08-20 19:14:51 -0300

    Reorganizacion temporal para el menu

--- resources/views/shared/menu.blade.php
+++ resources/views/shared/menu.blade.php
@@ -1,15 +1,15 @@
         <div class="col-md-3 spark-settings-tabs">
             <aside>
-                <h3 class="nav-heading ">
-                    {{__('Consorcios')}} <a class="nav-link" href="{{ route('consorcios.index',null, false) }}">
-                        <i class="fa fa-fw text-left fa-btn fa-cog"></i>
-                    </a>
-                </h3>
-
                 <ul class="nav flex-column mb-4 ">
+                    <li class="nav-item">
+                        <a href="/settings/consorcios">+</a>
+                    </li>
                     <li class="nav-item ">
                         <a class="nav-link" href="/consorcios/1">
                             {{__('Consorcio1')}}
+                        </a>
+                        <a class="nav-link" href="/settings/consorcios/1">
+                            <i class="fa fa-fw text-left fa-btn fa-cog"></i>
                         </a>
                     </li>
                </ul>
