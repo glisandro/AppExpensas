f3a53cb03a5506a809d0e971eefa505813006889
DESKTOP-23QUUHO\Gustavo <gustavomartinez@gmail.com>
2018-09-16 09:46:59 -0300

    Test of the global scope Team in Consorcios

---
+++ tests/CreatesUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use App\Team;
+use App\User;
+
+trait CreatesUsers
+{
+    public function createUserWithTeam($data = []){
+
+        $user = factory(User::class)->create(array_merge([
+            'name' => 'Owner',
+            'email' => 'owner@appexpensas.com'
+        ], $data));
+
+        $user->teams()->attach(factory(Team::class)->create(['name' => 'Equipo 1', 'slug' => 'equipo-1']),['role' => 'owner']);
+
+        return $user;
+    }
+}
---
+++ tests/Unit/ConsorciosTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Consorcio;
+use App\Team;
+use App\User;
+use Tests\CreatesUsers;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ConsorciosTest extends TestCase
+{
+    use RefreshDatabase, CreatesUsers;
+
+    /** @test */
+    function if_the_team_global_scope_works()
+    {
+        $this->withoutExceptionHandling();
+
+        // Se registra con un equipo
+        $user = $this->createUserWithTeam();
+
+        // Ingresa al sitio
+        $this->actingAs($user);
+
+        // Crea el el segundo equipo
+        $team2 = factory(Team::class)->create(['name' => 'Equipo 2', 'slug' => 'equipo-2']);
+
+        $user->teams()->attach($team2,['role' => 'owner']);
+
+
+        // Crea un consorcio para su equipo por defecto
+        factory(Consorcio::class)->create([
+            'name' => 'Equipo 1',
+            'team_id' => $user->currentTeam()->id
+        ]);
+
+        // Crea un consorcio para su equipo por defecto
+        factory(Consorcio::class)->create([
+            'name' => 'Consorcio para el segundo equipo',
+            'team_id' => $team2->id
+        ]);
+
+        //Como esta logeado el global scope debe traer solo los de su "currentTeam"
+        $consorcios = Consorcio::all();
+
+        foreach ($consorcios as $consorcio) {
+            $this->assertTrue($consorcio->team_id === $user->currentTeam()->id);
+        }
+
+    }
+
+
+}
