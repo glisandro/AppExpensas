da5b4d39105803287da00a87f2c2c48036659c38
DESKTOP-23QUUHO\Gustavo <gustavomartinez@gmail.com>
2018-10-04 19:04:23 -0300

    Mejoras en el menú

---
+++ resources/views/shared/menu-global.blade.php
@@ -0,0 +1,16 @@
+<aside>
+    <ul class="nav flex-column mb-4">
+        <li class="nav-item">
+            <a class="nav-link" href="/settings/consorcios">+{{__('Nuevo Consorcio')}}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Opcion global 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Opcion global 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Opcion global 3</a>
+        </li>
+    </ul>
+</aside>
