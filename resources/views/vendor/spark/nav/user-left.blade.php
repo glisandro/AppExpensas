22fdc7f9d4703a475dc05584c952025eda8f4357
DESKTOP-23QUUHO\Gustavo <gustavomartinez@gmail.com>
2018-10-22 11:08:08 -0300

    Show Consorcio in top select

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -3,7 +3,11 @@
 <!-- Switch Current Team -->
 <div class="dropdown show">
     <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Consorcios
+        @if(! empty(request('consorcio')))
+            {{substr(request('consorcio')->name, 0, 15)}}
+        @else
+            {{__('Consorcios')}}
+        @endif
     </a>
 
     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
